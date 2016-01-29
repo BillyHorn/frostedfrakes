33204bb7f51450be37407698826adf5addb839a5
Will Perlichek <wperlichek@catalystitservices.com>
2016-01-29 11:18:54 -0800

    create project function

--- src/main/resources/public/app/ctrl/createReportCtrl.js
+++ src/main/resources/public/app/ctrl/createReportCtrl.js
@@ -1,28 +1,33 @@
 angular.module('app').controller('createReportCtrl', ['$scope', 'httpService', '$state', function($scope, httpService, $state) {
 	
-	$scope.reports = {};
-	#scope.reports.availableUsersForReport = {};
-	$scope.reports.availableProjectsForReport = {};
-	
-	/* And expense report has a name, a submitter, a
-	  project, an approver and line items. */
+
+    $scope.createReport = function(){
+        console.log($scope.report);
+        httpService.createReport($scope.report).then(function(){
+            $state.go('home');
+        }, function(response){
+            console.log(response);
+        });
+    };
 	
 	  httpService.getProjects().then(function(response){
+		    console.log(response);
 	        $scope.projects = response.data;
 	    });
 	  
 	  httpService.getUsers().then(function(response){
+		    console.log(response);
 	        $scope.users = response.data;
 	    });
 	  
 	 
 	  $scope.addSubmitter = function(){
-		  	
+		  	$scope.submitter = $scope.users.email;
 	  };
 	  
 	  
 	  $scope.addProject = function(){
-		 
+		    $scope.project = $scope.projects.name;
 	  };
 	 
 }]);
