26570df0b9b0bb7e2bdd7ee590de233f0ca53227
mpKness <mKness@catalystitservices.com>
2016-02-01 10:47:22 -0800

    removed duplicate file

--- target/classes/public/app/service/httpService.js
+++ target/classes/public/app/service/httpService.js
@@ -3,21 +3,19 @@ angular.module('app').service('httpService', ['$http', function($http){
         return $http.get('/users');
     }
 
-<<<<<<< HEAD
+
     function getReport() {
       return $http.get('/report:' + reportid);
-=======
+
     function createProject(data){
         return $http.post('/project/create', data);
->>>>>>> EFF-7
+
     }
 
     return {
         getUsers : getUsers,
-<<<<<<< HEAD
         getReport : getReport
-=======
         createProject : createProject
->>>>>>> EFF-7
+
     };
 }]);
