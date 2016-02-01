b208a898d06f5efaa3cb9d445a9a3a507b109f69
unknown <spallipatt@PC10356.catalystsolves.com>
2016-02-01 09:57:36 -0800

    correced registration

--- src/main/resources/public/app/app.js
+++ src/main/resources/public/app/app.js
@@ -1,19 +1,24 @@
-angular.module('app', ['ui.router']);
+angular.module('app', [ 'ui.router' ]);
 
-angular.module('app').config(['$stateProvider', '$urlRouterProvider',
-function($stateProvider, $urlRouterProvider){
+angular.module('app').config(
+		[ '$stateProvider', '$urlRouterProvider',
+				function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/home');
+					$urlRouterProvider.otherwise('/home');
 
-    $stateProvider
+					$stateProvider
 
-        .state('home', {
-            url: '/home',
-            templateUrl: 'views/partials/home.html',
-            controller: 'homeCtrl',
-        }).state('createProject', {
-            url: '/createProject',
-            templateUrl: 'views/partials/createProject.html',
-            controller: "createProjectCtrl"
-        });
-}]);
+					.state('home', {
+						url : '/home',
+						templateUrl : 'views/partials/home.html',
+						controller : 'homeCtrl',
+					}).state('createProject', {
+						url : '/createProject',
+						templateUrl : 'views/partials/createProject.html',
+						controller : "createProjectCtrl"
+					}).state('registration', {
+						url : '/registration',
+						templateUrl : 'views/partials/registration.html',
+						controller : "registrationCtrl"
+					});
+				} ]);
