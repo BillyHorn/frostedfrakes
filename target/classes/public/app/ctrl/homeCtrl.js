1c654b5c2a97625d771f56293c805814ee3897a0
Kevin Matthiesen <kmatthiesen@catalystitservices.com>
2016-02-04 09:16:55 -0800

    fixed nav bar

--- target/classes/public/app/ctrl/homeCtrl.js
+++ target/classes/public/app/ctrl/homeCtrl.js
@@ -1,12 +1,5 @@
 angular.module('app').controller('homeCtrl', ['$scope', 'currentUser', function($scope, currentUser) {
 
-    $scope.admin = function() {
-        $scope.currentUser = currentUser.getUser();
-        if ($scope.currentUser.role === 'admin'){
-            return true;
-        } else {
-            return false;
-        }
-    };
+    
 
 }]);
