84bd6aee61fe0371eb17398a772c4ae2a8a51c28
Will Perlichek <wperlichek@catalystitservices.com>
2016-02-09 14:23:35 -0800

    basic work on homeCtrl

--- src/main/resources/public/app/ctrl/homeCtrl.js
+++ src/main/resources/public/app/ctrl/homeCtrl.js
@@ -1,5 +1,8 @@
-angular.module('app').controller('homeCtrl', ['$scope', 'currentUser', function($scope, currentUser) {
+angular.module('app').controller('homeCtrl', ['$scope', 'getProjects','getReport', 'projectFinderService', 'stateConverterService', 'reportHttp', 'lineItemsHttp', 'getLineItems', 'getCategories','$state', 'savedState', 'submittedState',
+  function($scope, getProjects, getReport, projectFinderService, stateConverterService, reportHttp, lineItemsHttp,getLineItems, getCategories, $state, savedState, submittedState) {
 
-    
+
+	$scope.report = getReport.data;
+
 
 }]);
