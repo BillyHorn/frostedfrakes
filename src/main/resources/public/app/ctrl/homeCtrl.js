6224bdbe513b5577f3fb58e9f7d5a98d16f32416
Will Perlichek <wperlichek@catalystitservices.com>
2016-02-16 13:59:16 -0800

    admin can't see blank report fields

--- src/main/resources/public/app/ctrl/homeCtrl.js
+++ src/main/resources/public/app/ctrl/homeCtrl.js
@@ -4,18 +4,23 @@ angular.module('app').controller('homeCtrl', ['$scope', 'stateConverterService',
 	 /* obtain current user data from resolve */
      $scope.currentEmail = getThisUser.email;
 
+     $scope.roleCheck = getThisUser;
+     
+     /* if the current logged in user is not an admin, show all user reports on the home page */
+     if (getThisUser.role != "admin"){
+  
 	 /* @wPerlichek
-	   obtain all currentUsers resports via reportHttp and store
+	   obtain all currentUsers reports via reportHttp and store
 	   them in a scope variable that will be used to display the
 	   reports on the home page upon load */
 	 reportHttp.getReports($scope.currentEmail).then(function(response){
 		  $scope.thisUsersReports = response.data;
             for(var i = 0; i < $scope.thisUsersReports.length; i++){
-              /* convert from numeric representaton of "state" to string rep */
+              /* convert from numeric representation of "state" to string rep */
               $scope.thisUsersReports[i].state = stateConverterService.getString($scope.thisUsersReports[i].state);
             }
         }, function(response){
             console.log(response);
         });
-
+     }
 }]);
